1adf2791983e17919cfe62b4e2c894bc2d0c3ace
Ahmad <ahmadbnaboulsi@gmail.com>
2022-11-13 13:31:35 +0200

    Added computer choice and player choice functions

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    let a = Math.floor(Math.random()* 3)
+    if (a == 0) { 
+        return "Rock";
+    }
+    else if (a === 1){
+            return "Paper";
+        }
+    else {
+            return "Scissors";
+        }
+}
+
+function getPlayerChoice() {
+    let answer = prompt("Rock, Paper, or Scissors?");
+
+    if (answer.toUpperCase() === "ROCK"){
+        return "Rock";
+    }
+    else if (answer.toUpperCase() === "PAPER"){
+        return "Paper";
+    }
+    else if (answer.toUpperCase() === "SCISSORS"){
+        return "Scissors";
+    }
+    else {
+        alert("Choose rock, paper or scissors");
+        return getPlayerChoice();
+    }
+
+}
+
+
+
